138c548fca865178eb291675ced8b46808148a78
Hemanth <katumalahemu@gmail.com>
2026-03-06 20:50:12 +0530

    switch to selenium manager

--- attendance_selenium.py
+++ attendance_selenium.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 
@@ -13,19 +12,13 @@ def get_attendance(username, password):
     chrome_options = Options()
     
 
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222")
-
-    chrome_options.binary_location = "/usr/bin/google-chrome"
     
-    service = Service(ChromeDriverManager().install())
 
-
-    driver = webdriver.Chrome(service=service,  options=chrome_options)
-
+    driver = webdriver.Chrome( options=chrome_options)
 
     
     wait = WebDriverWait(driver, 50)
