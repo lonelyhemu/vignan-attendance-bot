90361c1cceaa072d5579a0d8482a51704833045d
Hemanth <katumalahemu@gmail.com>
2026-03-06 20:30:23 +0530

    fix chrome binary path for railway

--- attendance_selenium.py
+++ attendance_selenium.py
@@ -9,14 +9,23 @@ from selenium.webdriver.chrome.service import Service
 def get_attendance(username, password):
 
     chrome_options = Options()
-    service = Service(ChromeDriverManager().install())
+    
 
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+
+    chrome_options.binary_location = "/usr/bin/google-chrome"
+    
+    Service = Service(ChromeDriverManager().install())
+
 
     driver = webdriver.Chrome(service=service,  options=chrome_options)
+
+
+    
     wait = WebDriverWait(driver, 50)
 
     driver.get("https://webprosindia.com/vignanit/Default.aspx")
