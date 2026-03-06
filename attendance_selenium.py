779e38774dbff2c00d2954dfe45745f79a4b225c
Hemanth <katumalahemu@gmail.com>
2026-03-06 20:08:05 +0530

    add headless chrome support

--- attendance_selenium.py
+++ attendance_selenium.py
@@ -3,10 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 def get_attendance(username, password):
 
     chrome_options = Options()
+    service = Service(ChromeDriverManager().install())
 
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
