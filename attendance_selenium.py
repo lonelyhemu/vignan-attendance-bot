86dc61f96c61e0de1e0d14764c1aca381478315b
Hemanth <katumalahemu@gmail.com>
2026-03-06 20:22:36 +0530

    fix chromedriver service

--- attendance_selenium.py
+++ attendance_selenium.py
@@ -16,7 +16,7 @@ def get_attendance(username, password):
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
 
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(service=service,  options=chrome_options)
     wait = WebDriverWait(driver, 50)
 
     driver.get("https://webprosindia.com/vignanit/Default.aspx")
